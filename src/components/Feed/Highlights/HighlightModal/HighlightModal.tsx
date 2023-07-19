3393fa17dab0c2cf7f06846a08ba2418bc060f58
Alex <alextrieu52@gmail.com>
2023-07-18 18:54:23 -0700

    Add automatic image transition in story viewer with 5-second interval

--- src/components/Feed/Highlights/HighlightModal/HighlightModal.tsx
+++ src/components/Feed/Highlights/HighlightModal/HighlightModal.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Post } from "../../../../types/PostTypes";
 import styles from "./HighlightModal.module.css";
 
@@ -10,6 +10,7 @@ const HighlightModal: React.FC<HighlightModalProps> = ({ data }) => {
   const [currentImageIndex, setcurrentImageIndex] = useState(0);
   const [inputClicked, setInputClicked] = useState(false);
   const [inputValue, setInputVallue] = useState("");
+  const { storyImages = [] } = data.user;
 
   function handleInputChange(event: React.ChangeEvent<HTMLInputElement>) {
     const value = event.target.value;
@@ -20,36 +21,70 @@ const HighlightModal: React.FC<HighlightModalProps> = ({ data }) => {
     setInputClicked((prev) => !prev);
   }
 
+  useEffect(() => {
+    // Interval that advances the image index every second (1000ms)
+    const advanceInterval = setInterval(() => {
+      setcurrentImageIndex((prevIndex) => {
+        // If we're at the last image, don't advance
+        if (prevIndex >= storyImages.length - 1) {
+          return prevIndex;
+        }
+        // Otherwise, advance to the next image
+        return prevIndex + 1;
+      });
+    }, 5000);
+
+    // Clean up the interval on unmount
+    return () => clearInterval(advanceInterval);
+  }, [data.user.storyImages]);
+
+  function nextImage() {
+    setcurrentImageIndex((prevIndex) => {
+      if (prevIndex >= storyImages.length - 1) {
+        return prevIndex;
+      }
+      return prevIndex + 1;
+    });
+  }
+
   return (
     <div className={styles.modalContainer}>
-      <div className={styles.modalContent}>
-        <div className={styles.progressContainer}>
-          {data.user.storyImages &&
-            data.user.storyImages.map((image, index) => (
-              <div
-                key={index}
-                className={styles.progressBar}
-                style={{
-                  backgroundColor: index < currentImageIndex ? "white" : index === currentImageIndex ? "white" : "gray",
-                }}
-              ></div>
-            ))}
+      <div
+        className={styles.modalContent}
+        style={{ backgroundImage: `url(${data.user.storyImages && data.user.storyImages[currentImageIndex]})` }}
+        // style={{ backgroundImage: `url("https://picsum.photos/seed/0ehs1GqO/640/480")` }}
+        onClick={nextImage}
+      >
+        <div className={styles.modalTopSection}>
+          <div className={styles.progressContainer}>
+            {data.user.storyImages &&
+              data.user.storyImages.map((image, index) => (
+                <div
+                  key={index}
+                  className={styles.progressBar}
+                  style={{
+                    backgroundColor:
+                      index < currentImageIndex ? "white" : index === currentImageIndex ? "white" : "gray",
+                  }}
+                ></div>
+              ))}
+          </div>
+          <header className={styles.modalHeader}>
+            <div className={styles.userDetails}>
+              <img src={data.user.profilePic} alt="User Profile" />
+              <p>{data.user.username}</p>
+              <p>4 hr</p>
+            </div>
+            <div className={styles.modalControls}>
+              <i className="fa-solid fa-play"></i>
+              <i className="fa-solid fa-volume-xmark"></i>
+              <i className="fa-solid fa-ellipsis"></i>
+            </div>
+          </header>
         </div>
-        <header>
-          <div className={styles.userDetails}>
-            <img src={data.user.profilePic} alt="User Profile" />
-            <p>{data.user.username}</p>
-            <p>4 hr</p>
-          </div>
-          <div className={styles.modalControls}>
-            <i className="fa-solid fa-play"></i>
-            <i className="fa-solid fa-volume-xmark"></i>
-            <i className="fa-solid fa-ellipsis"></i>
-          </div>
-        </header>
-        <figure className={styles.imageContainer}>
+        {/* <figure className={styles.imageContainer}>
           <img src={data.user.storyImages && data.user.storyImages[currentImageIndex]} alt="Story" />
-        </figure>
+        </figure> */}
         <footer className={styles.modalFooter}>
           <input
             type="text"

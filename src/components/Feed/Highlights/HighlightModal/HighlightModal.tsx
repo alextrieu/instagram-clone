8aed22a81840090023d18e164f435fa94181e3b4
Alex <alextrieu52@gmail.com>
2023-09-29 14:11:35 -0700

    Implement dynamic width for progress bars

--- src/components/Feed/Highlights/HighlightModal/HighlightModal.tsx
+++ src/components/Feed/Highlights/HighlightModal/HighlightModal.tsx
@@ -5,8 +5,6 @@ import ProgressBar from "./ProgressBar";
 import Header from "./Header";
 import Footer from "./Footer";
 import Logo from "../../../../assets/Instagram-Wordmark-White-Logo.wine.png";
-// import HighlightPreview from "./HighlightPreview";
-// import HighlightCover from "../HighlightCover/HighlightCover";
 
 type HighlightModalProps = {
   currentStory: Post | null;
@@ -39,7 +37,7 @@ const HighlightModal: React.FC<HighlightModalProps> = ({
     const now = Date.now();
     if (startTime) {
       const timeElapsed = now - startTime;
-      setElapsedTime((prev) => prev + timeElapsed);
+      setElapsedTime(timeElapsed);
     }
   };
 
@@ -53,13 +51,30 @@ const HighlightModal: React.FC<HighlightModalProps> = ({
       remainingTime > 0 ? remainingTime : 0
     );
     setTimerId(id);
-    setStartTime(Date.now());
+    setStartTime(Date.now() - elapsedTime);
   };
 
   useEffect(() => {
     setElapsedTime(0);
-    setStartTime(Date.now());
+    if (!isPaused) {
+      setStartTime(Date.now());
+    }
   }, [currentStory]);
+
+  useEffect(() => {
+    let timer: ReturnType<typeof setTimeout>;
+
+    if (!isPaused && startTime !== null) {
+      timer = setInterval(() => {
+        const now = Date.now();
+        setElapsedTime(now - startTime);
+      }, 100);
+    }
+
+    return () => {
+      if (timer) clearInterval(timer);
+    };
+  }, [isPaused, startTime]);
 
   return (
     <div className={styles.modalContainer}>
@@ -83,6 +98,8 @@ const HighlightModal: React.FC<HighlightModalProps> = ({
           <ProgressBar
             storyImages={(currentStory && currentStory.user.storyImages) || []}
             currentImageIndex={currentStory?.currentSegment || 0}
+            elapsedTime={elapsedTime}
+            isPaused={isPaused}
           />
 
           {currentStory?.user && (
@@ -101,6 +118,7 @@ const HighlightModal: React.FC<HighlightModalProps> = ({
         <div className={styles.leftSideContainer}>
           {prevStories?.map((story) => (
             <div
+              key={story.id}
               className={styles.leftSideUsers}
               style={{
                 backgroundImage: `url(${story.user.storyImages && story.user.storyImages[0]})`,
@@ -118,6 +136,7 @@ const HighlightModal: React.FC<HighlightModalProps> = ({
         <div className={styles.rightSideContainer}>
           {nextStories?.map((story) => (
             <div
+              key={story.id}
               className={styles.rightSideUsers}
               style={{
                 backgroundImage: `url(${story.user.storyImages && story.user.storyImages[0]})`,
--- src/components/Feed/Highlights/HighlightModal/ProgressBar.tsx
+++ src/components/Feed/Highlights/HighlightModal/ProgressBar.tsx
@@ -1,23 +1,64 @@
 import styles from "./HighlightModal.module.css";
+import { useEffect, useState } from "react";
 
 type ProgressBarProps = {
   storyImages: string[];
   currentImageIndex: number;
+  elapsedTime: number;
+  isPaused: boolean;
 };
 
-const ProgressBar: React.FC<ProgressBarProps> = ({ storyImages, currentImageIndex }) => {
+const ProgressBar: React.FC<ProgressBarProps> = ({ storyImages, currentImageIndex, elapsedTime, isPaused }) => {
+  const [progressWidths, setProgressWidths] = useState<number[]>(Array(storyImages.length).fill(0));
+
+  useEffect(() => {
+    let timer: ReturnType<typeof setInterval>;
+    if (!isPaused) {
+      timer = setInterval(() => {
+        setProgressWidths((prevWidths) => {
+          const newWidths = [...prevWidths];
+          if (currentImageIndex < storyImages.length) {
+            newWidths[currentImageIndex] = ((elapsedTime % 4000) / 4000) * 100;
+          }
+          return newWidths;
+        });
+      }, 100);
+    }
+    return () => {
+      clearInterval(timer);
+    };
+  }, [isPaused, elapsedTime, currentImageIndex]);
+
+  useEffect(() => {
+    setProgressWidths(() => {
+      const newWidths = Array(storyImages.length).fill(0);
+      for (let i = 0; i < currentImageIndex; i++) {
+        newWidths[i] = 100;
+      }
+      return newWidths;
+    });
+  }, [storyImages.length, currentImageIndex]);
+
   return (
     <div className={styles.progressContainer}>
-      {storyImages &&
-        storyImages.map((_, index) => (
+      {storyImages.map((_, index) => (
+        <div
+          key={index}
+          className={styles.progressBar}
+          style={{
+            backgroundColor: "gray",
+          }}
+        >
           <div
-            key={index}
-            className={styles.progressBar}
             style={{
-              backgroundColor: index < currentImageIndex ? "white" : index === currentImageIndex ? "white" : "gray",
+              width: `${progressWidths[index] || 0}%`,
+              backgroundColor: index <= currentImageIndex ? "white" : "gray",
+              height: "100%",
+              transition: "width 0.2s",
             }}
           ></div>
-        ))}
+        </div>
+      ))}
     </div>
   );
 };
